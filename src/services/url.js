cb3853713959e4d27b31b42a213a0c84bb59bd99
Phuong <phuongphuong262204@gmail.com>
2024-05-08 15:25:52 +0700

    update

--- src/services/url.js
+++ src/services/url.js
@@ -1,28 +1,19 @@
 const url = {
     BASE_URL: "https://localhost:7270/api/",
 
-    CONTACT: {
-        CREATE: "feedback/create",
-    },
+
 
     ARTIST: {
-        LIST: "shop/get-all",
-        DETAIL: "shop/detail/{}",
-        RELATEDS: "shop/relateds/{}",
-        GETBYFLOOR: "shop/get-all-by-floor/{}",
-        GETBYCATEGORY: "shop/get-all-by-category/{}",
+
     },
 
     ARTWORK: {
-        LIST: "floor/get-all",
+
     },
 
     PAYPAL: {
         DETAIL: "Offers/detailForUser"
     },
 
-    GALLERY: {
-        LIST: "/gallery/get-all",
-    },
 }; 
 export default url;
