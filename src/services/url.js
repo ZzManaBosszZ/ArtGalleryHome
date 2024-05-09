35e2fa8681a69cd049c0d9b8bc66c2d02d13ead7
Phuong <phuongphuong262204@gmail.com>
2024-05-09 21:56:45 +0700

    update api URL

--- src/services/url.js
+++ src/services/url.js
@@ -1,14 +1,36 @@
 const url = {
     BASE_URL: "https://localhost:7270/api/",
 
-
+    AUTH: {
+        REGISTER: "AUTH/register",
+        LOGIN: "AUTH/login",
+        FORGOT_PASSWORD: "AUTH/forgot-password",
+        PROFILE: "AUTH/profile",
+        UPDATE_PROFILE: "AUTH/update-profile",
+        CHANGE_PASSWORD: "AUTH/change-password",
+        RESET_PASSWORD: "AUTH/reset-password",
+    },
 
     ARTIST: {
-
+        LIST:"Artists",
+        DETAIL: "Artists/{}",
     },
 
     ARTWORK: {
+        LIST:"ArtWorks",
+        DETAIL: "ArtWorks/{}",
+    },
 
+    OFFER: {
+        CREATE: "Offers/CreateOfferUser",
+        MY_OFFER: "Offers/get-by-user",
+        DETAIL: "Offers/detailForUser"
+    },
+
+    FAVORITE: {
+        BY_USER: "Favorites/get-by-user",
+        ADD: "Favorites/addtofavorite",
+        REMOVE: "Favorites/removefavorite",
     },
 
     PAYPAL: {
